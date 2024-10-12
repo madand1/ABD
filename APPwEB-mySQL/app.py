2c0938088c99ca9ff3ca5bb2ad5fa101b15570e3
madand1 <asirandy@gmail.com>
2024-10-12 17:15:20 +0200

    cambi en batmanSQL

--- APPwEB-mySQL/app.py
+++ APPwEB-mySQL/app.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import pymysql
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Necesario para los mensajes flash
+app.secret_key = 'supersecretkey'  
 
-# Ruta principal: muestra el formulario para ingresar los datos de conexión
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Obtenemos los datos enviados por el formulario
+        
         db_host = request.form['db_host']
         db_user = request.form['db_user']
         db_password = request.form['db_password']
         db_name = request.form['db_name']
 
-        # Intentamos conectarnos a la base de datos
+        
         try:
             connection = pymysql.connect(host=db_host, user=db_user, password=db_password, db=db_name)
             connection.close()
@@ -25,7 +24,7 @@ def index():
 
     return render_template('index.html')
 
-# Ruta para mostrar las tablas de la base de datos si la conexión es exitosa
+
 @app.route('/tablas')
 def tablas():
     db_host = request.args.get('db_host')
@@ -41,7 +40,7 @@ def tablas():
 
     return render_template('tablas.html', tables=tables, db_host=db_host, db_user=db_user, db_password=db_password, db_name=db_name)
 
-# Ruta para mostrar los registros de una tabla seleccionada
+
 @app.route('/mostrar_tabla/<nombre_tabla>')
 def mostrar_tabla(nombre_tabla):
     db_host = request.args.get('db_host')
@@ -52,11 +51,11 @@ def mostrar_tabla(nombre_tabla):
     connection = pymysql.connect(host=db_host, user=db_user, password=db_password, db=db_name)
     cursor = connection.cursor()
 
-    # Obtener los registros de la tabla
+    
     cursor.execute(f"SELECT * FROM {nombre_tabla};")
     registros = cursor.fetchall()
 
-    # Obtener los nombres de las columnas
+    
     column_names = [desc[0] for desc in cursor.description]
 
     connection.close()
